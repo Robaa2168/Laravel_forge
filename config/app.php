8f8ebaee7e2e1bd452fb6902f7b3182312c771cc
robaa2168 <robertlagat38@gmail.com>
2022-08-07 06:57:03 -0700

    first

--- config/app.php
+++ config/app.php
@@ -55,9 +55,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://siasakenya.com'),
 
-    'asset_url' => env('ASSET_URL', 'http://localhost/Files/core/public'),
+    'asset_url' => env('ASSET_URL', 'http://siasakenya.com/'),
 
     /*
     |--------------------------------------------------------------------------
